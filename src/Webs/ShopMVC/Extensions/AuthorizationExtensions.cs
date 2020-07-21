30b35c4e84c3b1052228f12acad7fa208f62027d
Patryk W <patryk836@o2.pl>
2020-07-21 03:50:01 +0200

    AddShopMVC&ChangeHomeIndexView

---
+++ src/Webs/ShopMVC/Extensions/AuthorizationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopMVC.Extensions
+{
+    public static class AuthorizationExtensions
+    {
+        public static void AddCustomAuth(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie();
+
+            services.AddAuthorization(options =>
+            {
+               
+            });
+        }
+    }
+}
