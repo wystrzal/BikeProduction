30affa63c556a5592c4d879f9d2bd1fac3d478b2
Patryk W <patryk836@o2.pl>
2020-08-09 21:06:48 +0200

    HttpClient-AddQueryParamsToGetStringAction

--- src/Shared/HTTP/HttpClient/CustomHttpClient.cs
+++ src/Shared/HTTP/HttpClient/CustomHttpClient.cs
@@ -18,21 +18,27 @@ namespace BikeHttpClient
             this.client = new System.Net.Http.HttpClient();
         }
 
-        public async Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
+        public async Task<string> GetStringAsync(string uri, string authorizationToken = null, Dictionary<string, string> queryParams = null)
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
 
             // SetAuthorizationHeader(requestMessage);
             if (authorizationToken != null)
             {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authorizationMethod, authorizationToken);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authorizationToken);
             }
+
+            if (queryParams != null)
+            {
+                requestMessage.Content = new FormUrlEncodedContent(queryParams);
+            }
+
             var response = await client.SendAsync(requestMessage);
 
             return await response.Content.ReadAsStringAsync();
         }
 
-        private async Task<HttpResponseMessage> DoPostPutAsync<T>(HttpMethod method, string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        private async Task<HttpResponseMessage> DoPostPutAsync<T>(HttpMethod method, string uri, T item, string authorizationToken = null)
         {
             if (method != HttpMethod.Post && method != HttpMethod.Put)
             {
@@ -45,7 +51,7 @@ namespace BikeHttpClient
 
             if (authorizationToken != null)
             {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authorizationMethod, authorizationToken);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authorizationToken);
             }
 
             var response = await client.SendAsync(requestMessage);
@@ -59,22 +65,22 @@ namespace BikeHttpClient
         }
 
 
-        public async Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        public async Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null)
         {
-            return await DoPostPutAsync(HttpMethod.Post, uri, item, authorizationToken, authorizationMethod);
+            return await DoPostPutAsync(HttpMethod.Post, uri, item, authorizationToken);
         }
 
-        public async Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer")
+        public async Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationToken = null)
         {
-            return await DoPostPutAsync(HttpMethod.Put, uri, item, authorizationToken, authorizationMethod);
+            return await DoPostPutAsync(HttpMethod.Put, uri, item, authorizationToken);
         }
-        public async Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
+        public async Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null)
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Delete, uri);
 
             if (authorizationToken != null)
             {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authorizationMethod, authorizationToken);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authorizationToken);
             }
 
             return await client.SendAsync(requestMessage);
--- src/Shared/HTTP/HttpClient/ICustomHttpClient.cs
+++ src/Shared/HTTP/HttpClient/ICustomHttpClient.cs
@@ -8,9 +8,9 @@ namespace BikeHttpClient
 {
     public interface ICustomHttpClient
     {
-        Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer");
-        Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer");
-        Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer");
-        Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationToken = null, string authorizationMethod = "Bearer");
+        Task<string> GetStringAsync(string uri, string authorizationToken = null, Dictionary<string, string> queryParams = null);
+        Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationToken = null);
+        Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null);
+        Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationToken = null);
     }
 }
