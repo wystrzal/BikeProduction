c3b3fcc87cef6a9e8f5cac2d41d0b65b6fa95208
Patryk W <patryk836@o2.pl>
2020-08-13 03:12:17 +0200

    ShopMVC-AddEnumExtensions(SelectListItem)

---
+++ src/Webs/ShopMVC/Extensions/EnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopMVC.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static IEnumerable<SelectListItem> SelectListItem(this Enum @enum)
+        {
+            var enumType = @enum.GetType();
+
+            return Enum.GetNames(enumType).Select(x => new SelectListItem() { Text = x, Value = x });
+        }
+    }
+}
