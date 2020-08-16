790ffd0159cfede51b8c98c5fe329e2fe4935819
Patryk W <patryk836@o2.pl>
2020-08-16 16:33:41 +0200

    ShopMVC-UpdateSortEnum

--- src/Webs/ShopMVC/Models/Enums/SortEnum.cs
+++ src/Webs/ShopMVC/Models/Enums/SortEnum.cs
@@ -11,7 +11,9 @@ namespace ShopMVC.Models.Enums
             Price_Ascending = 1,
             Price_Descending = 2,
             Oldest_Added = 3,
-            Latest_Added = 4
+            Latest_Added = 4,
+            The_Most_Popular = 5,
+            The_Least_Popular = 6
         }
     }
 }
