4d2f451f430aa7c2cccb8119ad1b9d5f94424884
Patryk W <patryk836@o2.pl>
2020-08-22 01:31:31 +0200

    Catalog-TestGetProductQueryHandler

--- src/Services/Catalog/Catalog.Test/QueriesTest.cs
+++ src/Services/Catalog/Catalog.Test/QueriesTest.cs
@@ -2,6 +2,7 @@
 using Catalog.Application.Mapping;
 using Catalog.Application.Queries;
 using Catalog.Application.Queries.Handlers;
+using Catalog.Core.Exceptions;
 using Catalog.Core.Interfaces;
 using Catalog.Core.Models;
 using Catalog.Core.SearchSpecification;
@@ -9,6 +10,7 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,11 +22,13 @@ namespace Catalog.Test
     {
         private readonly Mock<IMapper> mapper;
         private readonly Mock<ISearchProductService> searchProductService;
+        private readonly Mock<IProductRepository> productRepository;
 
         public QueriesTest()
         {
             mapper = new Mock<IMapper>();
             searchProductService = new Mock<ISearchProductService>();
+            productRepository = new Mock<IProductRepository>();
         }
 
         [Fact]
@@ -48,6 +52,51 @@ namespace Catalog.Test
             //Assert
             Assert.Equal(2, action.Count());
             Assert.Equal(1, action.Select(x => x.Id).First());
+        }
+
+        [Fact]
+        public async Task GetProductQueryHandler_Success()
+        {
+            //Arrange
+            int productId = 1;
+            string productName = "test";
+
+            var product = new Product { Id = productId, ProductName = productName };
+            var productDto = new GetProductDto { ProductName = productName };
+
+            var query = new GetProductQuery(productId);
+
+            productRepository.Setup(x =>
+                x.GetByConditionWithIncludeFirst(It.IsAny<Func<Product, bool>>(), It.IsAny<Expression<Func<Product, Brand>>>()))
+                .Returns(Task.FromResult(product));
+
+            mapper.Setup(x => x.Map<GetProductDto>(product)).Returns(productDto);
+
+            var queryHandler = new GetProductQueryHandler(productRepository.Object, mapper.Object);
+
+            //Act
+            var action = await queryHandler.Handle(query, It.IsAny<CancellationToken>());
+
+            //Assert
+            Assert.Equal(productName, action.ProductName);
+        }
+
+        [Fact]
+        public async Task GetProductQueryHandler_ProductNotFoundException()
+        {
+            //Arrange
+            int productId = 1;
+
+            var query = new GetProductQuery(productId);
+
+            productRepository.Setup(x =>
+                x.GetByConditionWithIncludeFirst(It.IsAny<Func<Product, bool>>(), It.IsAny<Expression<Func<Product, Brand>>>()))
+                .Returns(Task.FromResult((Product)null));
+
+            var queryHandler = new GetProductQueryHandler(productRepository.Object, mapper.Object);
+
+            //Assert
+            await Assert.ThrowsAsync<ProductNotFoundException>(() => queryHandler.Handle(query, It.IsAny<CancellationToken>()));
         }
     }
 }
