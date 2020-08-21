3ff5f6f0166de93e5c501b2646cdf2c32c4de298
Patryk W <patryk836@o2.pl>
2020-08-21 03:50:41 +0200

    Catalog-TestAddProductCommandHandler

--- src/Services/Catalog/Catalog.Test/CommandsTest.cs
+++ src/Services/Catalog/Catalog.Test/CommandsTest.cs
@@ -1,4 +1,5 @@
-﻿using Catalog.Application.Commands;
+﻿using AutoMapper;
+using Catalog.Application.Commands;
 using Catalog.Application.Commands.Handlers;
 using Catalog.Core.Exceptions;
 using Catalog.Core.Interfaces;
@@ -20,11 +21,13 @@ namespace Catalog.Test
     {
         private readonly Mock<IProductRepository> productRepository;
         private readonly Mock<IBus> bus;
+        private readonly Mock<IMapper> mapper;
 
         public CommandsTest()
         {
             productRepository = new Mock<IProductRepository>();
             bus = new Mock<IBus>();
+            mapper = new Mock<IMapper>();
         }
 
         [Fact]
@@ -62,6 +65,30 @@ namespace Catalog.Test
 
             //Assert
             await Assert.ThrowsAsync<ProductNotFoundException>(() => commandHandler.Handle(command, It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task AddProductCommandHandler_Success()
+        {
+            //Arrange
+            var command = new AddProductCommand();
+            var product = new Product();
+
+            mapper.Setup(x => x.Map<Product>(command)).Returns(product);
+
+            productRepository.Setup(x => x.Add(product));
+            productRepository.Setup(x => x.SaveAllAsync()).Returns(Task.FromResult(true));
+
+            bus.Setup(x => x.Publish(It.IsAny<ProductAddedEvent>(), It.IsAny<CancellationToken>())).Verifiable();
+
+            var commandHandler = new AddProductCommandHandler(productRepository.Object, mapper.Object, bus.Object);
+
+            //Act
+            var action = await commandHandler.Handle(command, It.IsAny<CancellationToken>());
+
+            //Assert
+            Assert.Equal(Unit.Value, action);
+            bus.Verify(x => x.Publish(It.IsAny<ProductAddedEvent>(), It.IsAny<CancellationToken>()), Times.Once);
         }
     }
 }
