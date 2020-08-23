4e359ab0cb6ade80b61668e96bee3ee5e134c0ad
Patryk W <patryk836@o2.pl>
2020-08-23 03:41:22 +0200

    Basket_ControllerTest-TestChangeProductQuantity

---
+++ src/Services/Basket/Basket.Test/ControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using Basket.API.Controllers;
+using Basket.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Basket.Test
+{
+    public class ControllerTest
+    {
+        private readonly Mock<IMediator> mediator;
+        private readonly Mock<ILogger<BasketController>> logger;
+
+        public ControllerTest()
+        {
+            mediator = new Mock<IMediator>();
+            logger = new Mock<ILogger<BasketController>>();
+        }
+
+        [Fact]
+        public async Task ChangeProductQuantity_OkResult()
+        {
+            //Arrange
+            mediator.Setup(x => x.Send(It.IsAny<ChangeProductQuantityCommand>(), It.IsAny<CancellationToken>())).Verifiable();
+
+            var controller = new BasketController(mediator.Object, logger.Object);
+
+            //Act
+            var action = await controller.ChangeProductQuantity(It.IsAny<ChangeProductQuantityCommand>()) as OkResult;
+
+            //Assert
+            mediator.Verify(x => x.Send(It.IsAny<ChangeProductQuantityCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+            Assert.Equal(200, action.StatusCode);
+        }
+
+        [Fact]
+        public async Task ChangeProductQuantity_BadRequestObjectResult()
+        {
+            //Arrange
+            mediator.Setup(x => x.Send(It.IsAny<ChangeProductQuantityCommand>(), It.IsAny<CancellationToken>()))
+                .Throws(new Exception());
+
+            var controller = new BasketController(mediator.Object, logger.Object);
+
+            //Act
+            var action = await controller.ChangeProductQuantity(It.IsAny<ChangeProductQuantityCommand>()) as BadRequestObjectResult;
+
+            //Assert
+            Assert.Equal(400, action.StatusCode);
+            Assert.NotNull(action.Value);
+        }
+    }
+}
