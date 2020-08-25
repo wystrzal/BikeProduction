a99f6af139d6b9b6a2590d5c1a8bd2edd071feaf
Patryk W <patryk836@o2.pl>
2020-08-25 16:24:02 +0200

    Basket-TestGetBasketQueryHandler

---
+++ src/Services/Basket/Basket.Test/QueriesTest.cs
@@ -0,0 +1,43 @@
+﻿using Basket.Application.Queries;
+using Basket.Application.Queries.Handlers;
+using Basket.Core.Dtos;
+using Basket.Core.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Basket.Test
+{
+    public class QueriesTest
+    {
+        private readonly Mock<IBasketRedisService> basketRedisService;
+
+        public QueriesTest()
+        {
+            basketRedisService = new Mock<IBasketRedisService>();
+        }
+
+        [Fact]
+        public async Task GetBasketQueryHandler_Success()
+        {
+            //Arrange 
+            var userId = "1";
+            var query = new GetBasketQuery(userId);
+            var userBasketDto = new UserBasketDto { UserId = userId };
+
+            basketRedisService.Setup(x => x.GetBasket(userId)).Returns(Task.FromResult(userBasketDto));
+
+            var queryHandler = new GetBasketQueryHandler(basketRedisService.Object);
+
+            //Act
+            var action = await queryHandler.Handle(query, It.IsAny<CancellationToken>());
+
+            //Assert
+            Assert.Equal(userId, action.UserId);
+        }
+    }
+}
