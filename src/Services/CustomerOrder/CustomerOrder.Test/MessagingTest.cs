aefc4effd00ded36cbeb9a3dc4d356318aeeb934
Patryk W <patryk836@o2.pl>
2020-08-29 00:35:44 +0200

    CustomerOrder-TestChangeOrderStatusConsumer

---
+++ src/Services/CustomerOrder/CustomerOrder.Test/MessagingTest.cs
@@ -0,0 +1,68 @@
+﻿using Common.Application.Messaging;
+using CustomerOrder.Application.Messaging.Consumers;
+using CustomerOrder.Core.Exceptions;
+using CustomerOrder.Core.Interfaces;
+using CustomerOrder.Core.Models;
+using MassTransit;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static CustomerOrder.Core.Models.Enums.OrderStatusEnum;
+
+namespace CustomerOrder.Test
+{
+    public class MessagingTest
+    {
+        private readonly Mock<IOrderRepository> orderRepository;
+
+        public MessagingTest()
+        {
+            orderRepository = new Mock<IOrderRepository>();
+        }
+
+        [Fact]
+        public async Task ChangeOrderStatusConsumer_ThrowOrderNotFoundException()
+        {
+            //Arrange
+            var id = 1;
+
+            var changeOrderStatusEvent = new ChangeOrderStatusEvent(id, OrderStatus.Confirmed);
+
+            var context = Mock.Of<ConsumeContext<ChangeOrderStatusEvent>>(x => x.Message == changeOrderStatusEvent);
+
+            orderRepository.Setup(x => x.GetById(id)).Throws(new OrderNotFoundException());
+
+            var consumer = new ChangeOrderStatusConsumer(orderRepository.Object);
+
+            //Assert
+            await Assert.ThrowsAsync<OrderNotFoundException>(() => consumer.Consume(context));
+        }
+
+        [Fact]
+        public async Task ChangeOrderStatusConsumer_Success()
+        {
+            //Arrange
+            var id = 1;
+            var status = OrderStatus.Confirmed;
+            var order = new Order { OrderId = id };
+            var changeOrderStatusEvent = new ChangeOrderStatusEvent(id, status);
+
+            var context = Mock.Of<ConsumeContext<ChangeOrderStatusEvent>>(x => x.Message == changeOrderStatusEvent);
+
+            orderRepository.Setup(x => x.GetById(id)).Returns(Task.FromResult(order));
+            orderRepository.Setup(x => x.SaveAllAsync()).Verifiable();
+
+            var consumer = new ChangeOrderStatusConsumer(orderRepository.Object);
+
+            //Act
+            await consumer.Consume(context);
+
+            //Assert
+            Assert.Equal(status, order.OrderStatus);
+            orderRepository.Verify(x => x.SaveAllAsync(), Times.Once);
+        }
+    }
+}
