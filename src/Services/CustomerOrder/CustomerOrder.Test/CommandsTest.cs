9228118feb9798d2ddf7a4f3634c78c6b9dc359b
Patryk W <patryk836@o2.pl>
2020-08-28 23:24:33 +0200

    CustomerOrder-TestCreateOrderCommandHandler

--- src/Services/CustomerOrder/CustomerOrder.Test/CommandsTest.cs
+++ src/Services/CustomerOrder/CustomerOrder.Test/CommandsTest.cs
@@ -1,4 +1,5 @@
-﻿using Common.Application.Messaging;
+﻿using AutoMapper;
+using Common.Application.Messaging;
 using CustomerOrder.Application.Commands;
 using CustomerOrder.Application.Commands.Handlers;
 using CustomerOrder.Core.Exceptions;
@@ -22,11 +23,13 @@ namespace CustomerOrder.Test
     {
         private readonly Mock<IOrderRepository> orderRepository;
         private readonly Mock<IBus> bus;
+        private readonly Mock<IMapper> mapper;
 
         public CommandsTest()
         {
             orderRepository = new Mock<IOrderRepository>();
             bus = new Mock<IBus>();
+            mapper = new Mock<IMapper>();
         }
 
         [Fact]
@@ -100,6 +103,51 @@ namespace CustomerOrder.Test
             orderRepository.Verify(x => x.Delete(order), Times.Once);
             orderRepository.Verify(x => x.SaveAllAsync(), Times.Once);
             Assert.Equal(Unit.Value, action);
+        }
+
+        [Fact]
+        public async Task CreateOrderCommandHandler_ThrowOrderNotAddedException()
+        {
+            //Arrange
+            var command = new CreateOrderCommand();
+            var order = new Order();
+
+            mapper.Setup(x => x.Map<Order>(command)).Returns(order);
+
+            orderRepository.Setup(x => x.SaveAllAsync()).Returns(Task.FromResult(false));
+
+            var commandHandler = new CreateOrderCommandHandler(mapper.Object, orderRepository.Object, bus.Object);
+
+            //Assert
+            await Assert.ThrowsAsync<OrderNotAddedException>(() => 
+                commandHandler.Handle(command, It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task CreateOrderCommandHandler_Success()
+        {
+            //Arrange
+            var command = new CreateOrderCommand();
+            var order = new Order();
+
+            mapper.Setup(x => x.Map<Order>(command)).Returns(order);
+
+            orderRepository.Setup(x => x.Add(order)).Verifiable();
+
+            orderRepository.Setup(x => x.SaveAllAsync()).Returns(Task.FromResult(true));
+
+            bus.Setup(x => x.Publish(It.IsAny<OrderCreatedEvent>(), It.IsAny<CancellationToken>())).Verifiable();
+
+            var commandHandler = new CreateOrderCommandHandler(mapper.Object, orderRepository.Object, bus.Object);
+
+            //Act
+            var action = await commandHandler.Handle(command, It.IsAny<CancellationToken>());
+
+            //Assert
+            orderRepository.Verify(x => x.Add(order), Times.Once);
+            bus.Verify(x => x.Publish(It.IsAny<OrderCreatedEvent>(), It.IsAny<CancellationToken>()), Times.Once);
+            Assert.Equal(Unit.Value, action);
+
         }
     }
 }
