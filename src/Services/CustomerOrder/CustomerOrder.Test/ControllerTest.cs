547019d0b482dbaff6c214355e3b8a10bc56f2f1
Patryk W <patryk836@o2.pl>
2020-08-28 04:01:17 +0200

    CustomerOrder_Controller-TestGetOrder

--- src/Services/CustomerOrder/CustomerOrder.Test/ControllerTest.cs
+++ src/Services/CustomerOrder/CustomerOrder.Test/ControllerTest.cs
@@ -1,5 +1,7 @@
 ﻿using CustomerOrder.API.Controllers;
 using CustomerOrder.Application.Commands;
+using CustomerOrder.Application.Mapping;
+using CustomerOrder.Application.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -52,5 +54,47 @@ namespace CustomerOrder.Test
             Assert.Equal(400, action.StatusCode);
             Assert.NotNull(action.Value);
         }
+
+        [Fact]
+        public async Task GetOrder_OkObjectResult()
+        {
+            //Arrange
+            int orderId = 1;
+
+            mediator.Setup(x => x.Send(It.IsAny<GetOrderQuery>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(new GetOrderDto { OrderId = orderId }));
+
+            var controller = new CustomerOrderController(mediator.Object);
+
+            //Act
+            var action = await controller.GetOrder(orderId) as OkObjectResult;
+            var value = action.Value as GetOrderDto;
+
+            //Assert
+            Assert.Equal(200, action.StatusCode);
+            Assert.Equal(orderId, value.OrderId);
+        }
+
+        [Fact]
+        public async Task GetOrder_BadRequestObjectResult()
+        {
+            //Arrange
+            int orderId = 1;
+
+            mediator.Setup(x => x.Send(It.IsAny<GetOrderQuery>(), It.IsAny<CancellationToken>()))
+                .Throws(new Exception());
+
+            var controller = new CustomerOrderController(mediator.Object);
+
+            //Act
+            var action = await controller.GetOrder(orderId) as BadRequestObjectResult;
+
+            //Assert
+            Assert.Equal(400, action.StatusCode);
+            Assert.NotNull(action.Value);
+        }
+
+
+
     }
 }
