1a99b168dd48b8a40fe4aa4b66b964bd7aef8156
Patryk W <patryk836@o2.pl>
2020-08-29 00:43:26 +0200

    CustomerOrder-TestGetOrderQueryHandler

---
+++ src/Services/CustomerOrder/CustomerOrder.Test/QueriesTest.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using CustomerOrder.Application.Mapping;
+using CustomerOrder.Application.Queries;
+using CustomerOrder.Application.Queries.Handlers;
+using CustomerOrder.Core.Exceptions;
+using CustomerOrder.Core.Interfaces;
+using CustomerOrder.Core.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CustomerOrder.Test
+{
+    public class QueriesTest
+    {
+        private readonly Mock<IOrderRepository> orderRepository;
+        private readonly Mock<IMapper> mapper;
+
+        public QueriesTest()
+        {
+            orderRepository = new Mock<IOrderRepository>();
+            mapper = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public async Task GetOrderQueryHandler_ThrowOrderNotFoundException()
+        {
+            //Arrange
+            orderRepository.Setup(x => x.GetByConditionWithIncludeFirst(It.IsAny<Func<Order, bool>>(),
+                It.IsAny<Expression<Func<Order, ICollection<OrderItem>>>>()))
+                .Returns(Task.FromResult((Order)null));
+
+            var queryHandler = new GetOrderQueryHandler(orderRepository.Object, mapper.Object);
+
+            //Assert
+            await Assert.ThrowsAsync<OrderNotFoundException>(() => 
+                queryHandler.Handle(It.IsAny<GetOrderQuery>(), It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task GetOrderQueryHandler_Success()
+        {
+            //Arrange
+            var id = 1;
+            var order = new Order { OrderId = id };
+            var orderDto = new GetOrderDto { OrderId = id };
+
+            orderRepository.Setup(x => x.GetByConditionWithIncludeFirst(It.IsAny<Func<Order, bool>>(),
+                It.IsAny<Expression<Func<Order, ICollection<OrderItem>>>>()))
+                .Returns(Task.FromResult(order));
+
+            mapper.Setup(x => x.Map<GetOrderDto>(order)).Returns(orderDto);
+
+            var queryHandler = new GetOrderQueryHandler(orderRepository.Object, mapper.Object);
+
+            //Act
+            var action = await queryHandler.Handle(It.IsAny<GetOrderQuery>(), It.IsAny<CancellationToken>());
+
+            //Assert
+            Assert.Equal(id, action.OrderId);
+        }
+    }
+}
