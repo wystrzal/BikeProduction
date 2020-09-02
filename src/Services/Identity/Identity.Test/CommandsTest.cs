7903553a1390f0b679677310f611c49d36f8864d
Patryk W <patryk836@o2.pl>
2020-09-02 22:56:45 +0200

    Identity-TestRegisterCommandHandler

--- src/Services/Identity/Identity.Test/CommandsTest.cs
+++ src/Services/Identity/Identity.Test/CommandsTest.cs
@@ -1,8 +1,10 @@
-﻿using Identity.Application.Commands;
+﻿using AutoMapper;
+using Identity.Application.Commands;
 using Identity.Application.Commands.Handlers;
 using Identity.Core.Exceptions;
 using Identity.Core.Interfaces;
 using Identity.Core.Models;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Moq;
@@ -35,10 +37,12 @@ namespace Identity.Test
         }
 
         private readonly Mock<ITokenService> tokenService;
+        private readonly Mock<IMapper> mapper;
 
         public CommandsTest()
         {
             tokenService = new Mock<ITokenService>();
+            mapper = new Mock<IMapper>();
         }
 
         [Fact]
@@ -103,6 +107,67 @@ namespace Identity.Test
 
             //Assert
             Assert.NotNull(action);
+        }
+
+        [Fact]
+        public async Task RegisterCommandHandler_ThrowsUserAlreadyExistException()
+        {
+            //Arrange
+            var userManager = GetMockUserManager();
+            var user = new User();
+            var command = new RegisterCommand { UserName = "user", Password = "User123" };
+
+            mapper.Setup(x => x.Map<User>(command)).Returns(user);
+
+            userManager.Setup(x => x.FindByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(user));
+
+            var commandHandler = new RegisterCommandHandler(mapper.Object, userManager.Object);
+
+            //Assert
+            await Assert.ThrowsAsync<UserAlreadyExistException>(() => commandHandler.Handle(command, It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task RegisterCommandHandler_ThrowsUserNotAddedException()
+        {
+            //Arrange
+            var userManager = GetMockUserManager();
+            var user = new User();
+            var command = new RegisterCommand { UserName = "user", Password = "User123" };
+
+            mapper.Setup(x => x.Map<User>(command)).Returns(user);
+
+            userManager.Setup(x => x.FindByNameAsync(It.IsAny<string>())).Returns(Task.FromResult((User)null));
+
+            userManager.Setup(x => x.CreateAsync(user, It.IsAny<string>())).Returns(Task.FromResult(IdentityResult.Failed()));
+
+            var commandHandler = new RegisterCommandHandler(mapper.Object, userManager.Object);
+
+            //Assert
+            await Assert.ThrowsAsync<UserNotAddedException>(() => commandHandler.Handle(command, It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public async Task RegisterCommandHandler_Success()
+        {
+            //Arrange
+            var userManager = GetMockUserManager();
+            var user = new User();
+            var command = new RegisterCommand { UserName = "user", Password = "User123" };
+
+            mapper.Setup(x => x.Map<User>(command)).Returns(user);
+
+            userManager.Setup(x => x.FindByNameAsync(It.IsAny<string>())).Returns(Task.FromResult((User)null));
+
+            userManager.Setup(x => x.CreateAsync(user, It.IsAny<string>())).Returns(Task.FromResult(IdentityResult.Success));
+
+            var commandHandler = new RegisterCommandHandler(mapper.Object, userManager.Object);
+
+            //Act
+            var action = await commandHandler.Handle(command, It.IsAny<CancellationToken>());
+
+            //Assert
+            Assert.Equal(Unit.Value, action);
         }
     }
 }
