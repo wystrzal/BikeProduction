4d62b07bc57916aa4fabd7432d677540ccd432c8
Patryk W <patryk836@o2.pl>
2020-09-09 21:32:32 +0200

    Production_ControllerTest-TestGetProductionQueues

--- src/Services/Production/Production.Test/ControllerTest.cs
+++ src/Services/Production/Production.Test/ControllerTest.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Production.API.Controllers;
 using Production.Application.Commands;
+using Production.Application.Mapping;
+using Production.Application.Queries;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -127,6 +129,27 @@ namespace Production.Test
             //Assert
             Assert.Equal(400, action.StatusCode);
             Assert.NotNull(action.Value);
+        }
+
+        [Fact]
+        public async Task GetProductionQueues_OkObjectResult()
+        {
+            //Arrange
+            IEnumerable<GetProductionQueuesDto> productionQueuesDto 
+                = new List<GetProductionQueuesDto> { new GetProductionQueuesDto(), new GetProductionQueuesDto() };
+
+            mediator.Setup(x => x.Send(It.IsAny<GetProductionQueuesQuery>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(productionQueuesDto));
+
+            var controller = new ProductionQueueController(mediator.Object);
+
+            //Act
+            var action = await controller.GetProductionQueues() as OkObjectResult;
+            var value = action.Value as List<GetProductionQueuesDto>;
+
+            //Assert
+            Assert.Equal(200, action.StatusCode);
+            Assert.Equal(2, value.Count);
         }
     }
 }
