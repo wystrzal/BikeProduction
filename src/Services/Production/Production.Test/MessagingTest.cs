a9bd218810e76bf4e98eb29ccb03559f7ca50725
Patryk W <patryk836@o2.pl>
2020-09-10 15:16:42 +0200

    Production-TestOrderCanceledConsumer

--- src/Services/Production/Production.Test/MessagingTest.cs
+++ src/Services/Production/Production.Test/MessagingTest.cs
@@ -62,5 +62,29 @@ namespace Production.Test
             //Assert
             await Assert.ThrowsAsync<ProductNotAddedException>(() => consumer.Consume(context));
         }
+
+        [Fact]
+        public async Task OrderCanceledConsumer_Success()
+        {
+            //Arrange
+            var id = 1;
+            var productionQueues = new List<ProductionQueue> { new ProductionQueue() };
+            var orderCanceledEvent = new OrderCanceledEvent(id);
+            var context = Mock.Of<ConsumeContext<OrderCanceledEvent>>(x => x.Message == orderCanceledEvent);
+
+            productionQueueRepo.Setup(x => x.GetByConditionToList(It.IsAny<Func<ProductionQueue, bool>>()))
+                .Returns(Task.FromResult(productionQueues));
+            productionQueueRepo.Setup(x => x.Delete(It.IsAny<ProductionQueue>())).Verifiable();
+            productionQueueRepo.Setup(x => x.SaveAllAsync()).Verifiable();
+
+            var consumer = new OrderCanceledConsumer(productionQueueRepo.Object);
+
+            //Act
+            await consumer.Consume(context);
+
+            //Assert
+            productionQueueRepo.Verify(x => x.Delete(It.IsAny<ProductionQueue>()), Times.Once);
+            productionQueueRepo.Verify(x => x.SaveAllAsync(), Times.Once);
+        }
     }
 }
