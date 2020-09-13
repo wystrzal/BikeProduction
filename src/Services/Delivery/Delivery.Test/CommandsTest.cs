432a5bfcd01c57a96596b4915c8fc284655a97b4
Patryk W <patryk836@o2.pl>
2020-09-13 14:49:46 +0200

    Delivery-TestStartDeliveryCommandHandler

--- src/Services/Delivery/Delivery.Test/CommandsTest.cs
+++ src/Services/Delivery/Delivery.Test/CommandsTest.cs
@@ -54,5 +54,32 @@ namespace Delivery.Test
             loadingPlaceRepo.Verify(x => x.SaveAllAsync(), Times.Once);
             Assert.Equal(Unit.Value, action);
         }
+
+        [Fact]
+        public async Task StartDeliveryCommandHandler_Success()
+        {
+            //Arrange
+            var id = 1;
+            var packsToDelivery = new List<PackToDelivery> { new PackToDelivery(), new PackToDelivery() };
+            var loadingPlace = new LoadingPlace { Id = id, PacksToDelivery = packsToDelivery };
+            var command = new StartDeliveryCommand(id);
+
+            loadingPlaceRepo.Setup(x => x.GetByConditionWithIncludeFirst(It.IsAny<Func<LoadingPlace, bool>>(),
+                It.IsAny<Expression<Func<LoadingPlace, ICollection<PackToDelivery>>>>())).Returns(Task.FromResult(loadingPlace));
+
+            bus.Setup(x => x.Publish(It.IsAny<ChangeOrderStatusEvent>(), It.IsAny<CancellationToken>())).Verifiable();
+
+            loadingPlaceRepo.Setup(x => x.SaveAllAsync()).Verifiable();
+
+            var commandHandler = new StartDeliveryCommandHandler(loadingPlaceRepo.Object, bus.Object);
+
+            //Act
+            var action = await commandHandler.Handle(command, It.IsAny<CancellationToken>());
+
+            //Assert
+            bus.Verify(x => x.Publish(It.IsAny<ChangeOrderStatusEvent>(), It.IsAny<CancellationToken>()), Times.Exactly(2));
+            loadingPlaceRepo.Verify(x => x.SaveAllAsync(), Times.Once);
+            Assert.Equal(Unit.Value, action);
+        }
     }
 }
