c4135582ad64c6a87e8c7e4554919b4fd4932474
Patryk W <patryk836@o2.pl>
2020-09-17 00:31:07 +0200

    Delivery-TestProductionFinishedConsumer

---
+++ src/Services/Delivery/Delivery.Test/Messaging/ProductionFinishedConsumerTest.cs
@@ -0,0 +1,78 @@
+﻿using Common.Application.Messaging;
+using Delivery.Application.Messaging.Consumers;
+using Delivery.Core.Interfaces;
+using Delivery.Core.Models;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Delivery.Test.Messaging
+{
+    public class ProductionFinishedConsumerTest
+    {
+        private readonly Mock<ICustomerOrderService> customerOrderService;
+        private readonly Mock<IPackToDeliveryRepo> packToDeliveryRepo;
+        private readonly Mock<ILogger<ProductionFinishedConsumer>> logger;
+
+        public ProductionFinishedConsumerTest()
+        {
+            customerOrderService = new Mock<ICustomerOrderService>();
+            packToDeliveryRepo = new Mock<IPackToDeliveryRepo>();
+            logger = new Mock<ILogger<ProductionFinishedConsumer>>();
+        }
+
+        [Fact]
+        public async Task ProductionFinishedConsumer_NullPack_Success()
+        {
+            //Arrange
+            var productionFinishedEvent = new ProductionFinishedEvent(1, 1);
+            var context = Mock.Of<ConsumeContext<ProductionFinishedEvent>>(x => x.Message == productionFinishedEvent);
+
+            packToDeliveryRepo.Setup(x => x.GetByConditionFirst(It.IsAny<Func<PackToDelivery, bool>>()))
+                .Returns(Task.FromResult((PackToDelivery)null));
+
+            customerOrderService.Setup(x => x.GetOrder(It.IsAny<int>())).Returns(Task.FromResult(new Order()));
+
+            packToDeliveryRepo.Setup(x => x.Add(It.IsAny<PackToDelivery>())).Verifiable();
+
+            packToDeliveryRepo.Setup(x => x.SaveAllAsync()).Verifiable();
+
+            var consumer = new ProductionFinishedConsumer(customerOrderService.Object, packToDeliveryRepo.Object, logger.Object);
+
+            //Act
+            await consumer.Consume(context);
+
+            //Assert
+            packToDeliveryRepo.Verify(x => x.Add(It.IsAny<PackToDelivery>()), Times.Once);
+            packToDeliveryRepo.Verify(x => x.SaveAllAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task ProductionFinishedConsumer_Success()
+        {
+            //Arrange
+            var productionFinishedEvent = new ProductionFinishedEvent(1, 1);
+            var context = Mock.Of<ConsumeContext<ProductionFinishedEvent>>(x => x.Message == productionFinishedEvent);
+            var packToDelivery = new PackToDelivery();
+
+            packToDeliveryRepo.Setup(x => x.GetByConditionFirst(It.IsAny<Func<PackToDelivery, bool>>()))
+                .Returns(Task.FromResult(packToDelivery));
+
+            packToDeliveryRepo.Setup(x => x.SaveAllAsync()).Verifiable();
+
+            var consumer = new ProductionFinishedConsumer(customerOrderService.Object, packToDeliveryRepo.Object, logger.Object);
+
+            //Act
+            await consumer.Consume(context);
+
+            //Assert
+            packToDeliveryRepo.Verify(x => x.SaveAllAsync(), Times.Once);
+        }
+    }
+}
