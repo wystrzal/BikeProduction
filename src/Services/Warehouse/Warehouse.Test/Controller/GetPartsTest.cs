9aca7e87891146e27d07666fd727b836b17042ef
Patryk W <patryk836@o2.pl>
2020-09-18 01:37:30 +0200

    Warehouse_Controller-TestGetParts

---
+++ src/Services/Warehouse/Warehouse.Test/Controller/GetPartsTest.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Warehouse.API.Controllers;
+using Warehouse.Application.Mapping;
+using Warehouse.Application.Queries;
+using Xunit;
+
+namespace Warehouse.Test.Controller
+{
+    public class GetPartsTest
+    {
+        private readonly Mock<IMediator> mediator;
+        private readonly Mock<ILogger<WarehouseController>> logger;
+
+        public GetPartsTest()
+        {
+            mediator = new Mock<IMediator>();
+            logger = new Mock<ILogger<WarehouseController>>();
+        }
+
+        [Fact]
+        public async Task GetParts_OkObjectResult()
+        {
+            //Arrange
+            IEnumerable<GetPartsDto> partsDto = new List<GetPartsDto> { new GetPartsDto(), new GetPartsDto() };
+
+            mediator.Setup(x => x.Send(It.IsAny<GetPartsQuery>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(partsDto));
+
+            var controller = new WarehouseController(mediator.Object, logger.Object);
+
+            //Act
+            var action = await controller.GetParts() as OkObjectResult;
+            var value = action.Value as List<GetPartsDto>;
+
+            //Assert
+            Assert.Equal(200, action.StatusCode);
+            Assert.Equal(2, value.Count);
+        }
+    }
+}
