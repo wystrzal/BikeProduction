eaf210e27e6b67cbf4eebe0523868f7dbfb9c463
Patryk W <patryk836@o2.pl>
2020-09-18 00:12:36 +0200

    Warehouse_Controller-TestAddPart

---
+++ src/Services/Warehouse/Warehouse.Test/Controller/AddPartTest.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Warehouse.API.Controllers;
+using Warehouse.Application.Commands;
+using Xunit;
+
+namespace Warehouse.Test.Controller
+{
+    public class AddPartTest
+    {
+        private readonly Mock<IMediator> mediator;
+
+        public AddPartTest()
+        {
+            mediator = new Mock<IMediator>();
+        }
+
+        [Fact]
+        public async Task AddPart_OkResult()
+        {
+            //Arrange
+            mediator.Setup(x => x.Send(It.IsAny<AddPartCommand>(), It.IsAny<CancellationToken>())).Verifiable();
+
+            var controller = new WarehouseController(mediator.Object);
+
+            //Act
+            var action = await controller.AddPart(It.IsAny<AddPartCommand>()) as OkResult;
+
+            //Assert
+            mediator.Verify(x => x.Send(It.IsAny<AddPartCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+            Assert.Equal(200, action.StatusCode);
+        }
+    }
+}
