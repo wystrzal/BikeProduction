7b574b64def7007c006b3825176eb263a0ab9580
Patryk W <patryk836@o2.pl>
2020-10-14 17:02:38 +0200

    Production-RefactoringOrderCanceledConsumer

--- src/Services/Production/Production.Application/Messaging/Consumers/OrderCanceledConsumer.cs
+++ src/Services/Production/Production.Application/Messaging/Consumers/OrderCanceledConsumer.cs
@@ -1,8 +1,11 @@
-﻿using Common.Application.Messaging;
+﻿using BikeBaseRepository;
+using Common.Application.Messaging;
 using MassTransit;
 using Microsoft.Extensions.Logging;
 using Production.Core.Interfaces;
+using Production.Core.Models;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Production.Application.Messaging.Consumers
@@ -19,24 +22,51 @@ namespace Production.Application.Messaging.Consumers
         }
         public async Task Consume(ConsumeContext<OrderCanceledEvent> context)
         {
-            var productionQueues = await productionQueueRepo.GetByConditionToList(x => x.OrderId == context.Message.OrderId);
-
-            foreach (var productionQueue in productionQueues)
-            {
-                productionQueueRepo.Delete(productionQueue);
-            }
-
             try
             {
-                await productionQueueRepo.SaveAllAsync();
+                ValidateContext(context);
+
+                var productionQueues = await GetProductionQueues(context.Message.OrderId);
+
+                await DeleteProductionQueues(productionQueues);
             }
             catch (Exception ex)
             {
                 logger.LogError(ex.Message);
                 throw;
             }
- 
+
             logger.LogInformation($"Successfully handled event: {context.MessageId} at {this} - {context}");
+        }
+
+        private void ValidateContext(ConsumeContext<OrderCanceledEvent> context)
+        {
+            if (context.Message.OrderId <= 0)
+            {
+                throw new ArgumentException("OrderID must be greater than zero.");
+            }
+        }
+
+        private async Task<List<ProductionQueue>> GetProductionQueues(int orderId)
+        {
+            var productionQueues = await productionQueueRepo.GetByConditionToList(x => x.OrderId == orderId);
+
+            if (productionQueues.Count <= 0)
+            {
+                throw new NullDataException();
+            }
+
+            return productionQueues;
+        }
+
+        private async Task DeleteProductionQueues(List<ProductionQueue> productionQueues)
+        {
+            foreach (var productionQueue in productionQueues)
+            {
+                productionQueueRepo.Delete(productionQueue);
+            }
+
+            await productionQueueRepo.SaveAllAsync();
         }
     }
 }
--- src/Services/Production/Production.Test/Messaging/OrderCanceledConsumerTest.cs
+++ src/Services/Production/Production.Test/Messaging/OrderCanceledConsumerTest.cs
@@ -31,8 +31,9 @@ namespace Production.Test.Messaging
         public async Task OrderCanceledConsumer_Success()
         {
             //Arrange
+            var id = 1;
             var productionQueues = new List<ProductionQueue> { new ProductionQueue(), new ProductionQueue() };
-            var orderCanceledEvent = new OrderCanceledEvent(It.IsAny<int>());
+            var orderCanceledEvent = new OrderCanceledEvent(id);
             var context = Mock.Of<ConsumeContext<OrderCanceledEvent>>(x => x.Message == orderCanceledEvent);
 
             productionQueueRepo.Setup(x => x.GetByConditionToList(It.IsAny<Func<ProductionQueue, bool>>()))
@@ -50,11 +51,45 @@ namespace Production.Test.Messaging
         }
 
         [Fact]
+        public async Task OrderCanceledConsumer_ThrowsNullDataException()
+        {
+            //Arrange
+            var id = 1;
+            var productionQueues = new List<ProductionQueue>();
+            var orderCanceledEvent = new OrderCanceledEvent(id);
+            var context = Mock.Of<ConsumeContext<OrderCanceledEvent>>(x => x.Message == orderCanceledEvent);
+
+            productionQueueRepo.Setup(x => x.GetByConditionToList(It.IsAny<Func<ProductionQueue, bool>>()))
+                .Returns(Task.FromResult(productionQueues));
+
+            var consumer = new OrderCanceledConsumer(productionQueueRepo.Object, logger.Object);
+
+            //Assert
+            await Assert.ThrowsAsync<NullDataException>(() => consumer.Consume(context));
+            logger.VerifyLogging(LogLevel.Error);
+        }
+
+        [Fact]
+        public async Task OrderCanceledConsumer_ThrowsArgumentException()
+        {
+            //Arrange
+            var orderCanceledEvent = new OrderCanceledEvent(It.IsAny<int>());
+            var context = Mock.Of<ConsumeContext<OrderCanceledEvent>>(x => x.Message == orderCanceledEvent);
+
+            var consumer = new OrderCanceledConsumer(productionQueueRepo.Object, logger.Object);
+
+            //Assert
+            await Assert.ThrowsAsync<ArgumentException>(() => consumer.Consume(context));
+            logger.VerifyLogging(LogLevel.Error);
+        }
+
+        [Fact]
         public async Task OrderCanceledConsumer_ThrowsChangesNotSavedCorrectlyException()
         {
             //Arrange
+            var id = 1;
             var productionQueues = new List<ProductionQueue> { new ProductionQueue(), new ProductionQueue() };
-            var orderCanceledEvent = new OrderCanceledEvent(It.IsAny<int>());
+            var orderCanceledEvent = new OrderCanceledEvent(id);
             var context = Mock.Of<ConsumeContext<OrderCanceledEvent>>(x => x.Message == orderCanceledEvent);
 
             productionQueueRepo.Setup(x => x.GetByConditionToList(It.IsAny<Func<ProductionQueue, bool>>()))
