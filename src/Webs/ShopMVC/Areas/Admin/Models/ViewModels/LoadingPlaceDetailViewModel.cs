84c0765ec418e240c6d9a745aa3bd813904fe8f0
Patryk W <patryk836@o2.pl>
2020-10-20 22:23:19 +0200

    ShopMVC-AdminArea-AddLoadingPlaceDetailView

---
+++ src/Webs/ShopMVC/Areas/Admin/Models/ViewModels/LoadingPlaceDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopMVC.Areas.Admin.Models.ViewModels
+{
+    public class LoadingPlaceDetailViewModel
+    {
+        public LoadingPlace LoadingPlace { get; set; }
+    }
+}
