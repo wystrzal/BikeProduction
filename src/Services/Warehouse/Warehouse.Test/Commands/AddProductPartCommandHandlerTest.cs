ff4da34b9fe148111ba5dd3e19345781794e5b8f
Patryk Wyprzał <patryk836@o2.pl>
2020-11-03 11:37:50 +0100

    Warehouse-TestAddProductPartAction

---
+++ src/Services/Warehouse/Warehouse.Test/Commands/AddProductPartCommandHandlerTest.cs
@@ -0,0 +1,66 @@
+﻿using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Warehouse.Application.Commands;
+using Warehouse.Application.Commands.Handlers;
+using Warehouse.Core.Interfaces;
+using Warehouse.Core.Models;
+using Xunit;
+
+namespace Warehouse.Test.Commands
+{
+    public class AddProductPartCommandHandlerTest
+    {
+        private const int id = 1;
+        private const string reference = "1";
+
+        private readonly Mock<IProductRepository> productRepository;
+        private readonly Mock<IProductPartRepo> productPartRepo;
+
+        private readonly AddProductPartCommand command;
+        private readonly AddProductPartCommandHandler commandHandler;
+        private readonly Product product;
+
+        public AddProductPartCommandHandlerTest()
+        {
+            productRepository = new Mock<IProductRepository>();
+            productPartRepo = new Mock<IProductPartRepo>();
+            command = new AddProductPartCommand(id, reference);
+            commandHandler = new AddProductPartCommandHandler(productRepository.Object, productPartRepo.Object);
+            product = new Product { Id = id };
+        }
+
+        [Fact]
+        public async Task AddProductPartCommandHandler_Success()
+        {
+            //Arrange
+            productRepository.Setup(x => x.GetByConditionFirst(It.IsAny<Func<Product, bool>>())).Returns(Task.FromResult(product));
+
+            //Act
+            var action = await commandHandler.Handle(command, It.IsAny<CancellationToken>());
+
+            //Assert
+            productPartRepo.Verify(x => x.Add(It.IsAny<ProductsParts>()), Times.Once);
+            productPartRepo.Verify(x => x.SaveAllAsync(), Times.Once);
+            Assert.Equal(Unit.Value, action);
+        }
+
+        [Fact]
+        public void AddProductPartCommandHandler_NullReference_ThrowsArgumentNullException()
+        {
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => new AddProductPartCommand(id, null));
+        }
+
+        [Fact]
+        public void AddProductPartCommandHandler_PartIdEqualZero_ThrowsArgumentException()
+        {
+            //Assert
+            Assert.Throws<ArgumentException>(() => new AddProductPartCommand(It.IsAny<int>(), reference));
+        }
+    }
+}
---
+++ src/Services/Warehouse/Warehouse.Test/Controller/AddProductPartTest.cs
@@ -0,0 +1,59 @@
+﻿using BikeExtensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Warehouse.API.Controllers;
+using Warehouse.Application.Commands;
+using Xunit;
+
+namespace Warehouse.Test.Controller
+{
+    public class AddProductPartTest
+    {
+        private const int id = 1;
+        private const string reference = "1";
+
+        private readonly Mock<IMediator> mediator;
+        private readonly Mock<ILogger<WarehouseController>> logger;
+        private readonly WarehouseController controller;
+
+        public AddProductPartTest()
+        {
+            mediator = new Mock<IMediator>();
+            logger = new Mock<ILogger<WarehouseController>>();
+            controller = new WarehouseController(mediator.Object, logger.Object);
+        }
+
+        [Fact]
+        public async Task AddProductPart_OkResult()
+        {
+            //Act
+            var action = await controller.AddProductPart(reference, id) as OkResult;
+
+            //Assert
+            mediator.Verify(x => x.Send(It.IsAny<AddProductPartCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+            Assert.Equal(200, action.StatusCode);
+        }
+
+        [Fact]
+        public async Task AddProductPart_BadRequestObjectResult()
+        {
+            //Arrange
+            mediator.Setup(x => x.Send(It.IsAny<AddProductPartCommand>(), It.IsAny<CancellationToken>())).Throws(new Exception());
+
+            //Act
+            var action = await controller.AddProductPart(reference, id) as BadRequestObjectResult;
+
+            //Assert
+            Assert.Equal(400, action.StatusCode);
+            Assert.NotNull(action.Value);
+            logger.VerifyLogging(LogLevel.Error);
+        }
+    }
+}
