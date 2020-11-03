c5d7bb4067af7671befb1da4614e35b9189a5b2f
Patryk Wyprzał <patryk836@o2.pl>
2020-11-03 11:57:12 +0100

    Identity-Add_DataAnnotationsToCommands

--- src/Services/Identity/Identity.Application/Commands/RegisterCommand.cs
+++ src/Services/Identity/Identity.Application/Commands/RegisterCommand.cs
@@ -1,10 +1,14 @@
 ﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace Identity.Application.Commands
 {
     public class RegisterCommand : IRequest
     {
+        [Required]
         public string UserName { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
--- src/Services/Identity/Identity.Application/Commands/TryLoginCommand.cs
+++ src/Services/Identity/Identity.Application/Commands/TryLoginCommand.cs
@@ -1,11 +1,15 @@
 ﻿using Identity.Core.Models;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace Identity.Application.Commands
 {
     public class TryLoginCommand : IRequest<TokenModel>
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
