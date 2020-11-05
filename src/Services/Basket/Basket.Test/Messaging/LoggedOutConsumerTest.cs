d2136e72b1f23c85c971e33f73cc7c1e7e88d6fa
Patryk Wyprzał <patryk836@o2.pl>
2020-11-05 11:39:05 +0100

    Basket-TestLoggedOutConsumer

---
+++ src/Services/Basket/Basket.Test/Messaging/LoggedOutConsumerTest.cs
@@ -0,0 +1,63 @@
+﻿using Basket.Application.Messaging.Consumers;
+using Basket.Core.Interfaces;
+using BikeExtensions;
+using Common.Application.Messaging;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Basket.Test.Messaging
+{
+    public class LoggedOutConsumerTest
+    {
+        private const string sessionId = "1";
+
+        private readonly Mock<IBasketRedisService> basketRedisService;
+        private readonly Mock<ILogger<LoggedOutConsumer>> logger;
+
+        private readonly LoggedOutConsumer consumer;
+
+        public LoggedOutConsumerTest()
+        {
+            basketRedisService = new Mock<IBasketRedisService>();
+            logger = new Mock<ILogger<LoggedOutConsumer>>();
+            consumer = new LoggedOutConsumer(basketRedisService.Object, logger.Object);
+        }
+
+        [Fact]
+        public async Task LoggedOutConsumer_NullSessionId_ThrowsArgumentNullException()
+        {
+            //Arrange
+            var context = GetContext(null);
+
+            //Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => consumer.Consume(context));
+            logger.VerifyLogging(LogLevel.Error);
+        }
+
+        [Fact]
+        public async Task LoggedOutConsumer_Success()
+        {
+            //Arrange
+            var context = GetContext();
+
+            //Act
+            await consumer.Consume(context);
+
+            //Assert
+            basketRedisService.Verify(x => x.RemoveBasket(sessionId), Times.Once);
+        }
+
+        private ConsumeContext<LoggedOutEvent> GetContext(string sessionId = sessionId)
+        {
+            var loggedOutEvent = new LoggedOutEvent { SessionId = sessionId };
+
+            return Mock.Of<ConsumeContext<LoggedOutEvent>>(x => x.Message == loggedOutEvent);
+        }
+    }
+}
