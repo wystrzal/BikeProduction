46fe6e8a38f76f532a4cdf02d7c468a26fdeb7a1
Patryk Wyprzał <patryk836@o2.pl>
2020-11-06 15:47:34 +0100

    Delivery-TestBaseLoadingPlaceCommand

---
+++ src/Services/Delivery/Delivery.Test/Commands/BaseLoadingPlaceCommandTest.cs
@@ -0,0 +1,19 @@
+﻿using Delivery.Application.Commands;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Delivery.Test.Commands
+{
+    public class BaseLoadingPlaceCommandTest
+    {
+        [Fact]
+        public void BaseLoadingPlaceCommand_NullLoadingPlaceId_ThrowsArgumentException()
+        {
+            //Assert
+            Assert.Throws<ArgumentException>(() => new DeleteLoadingPlaceCommand(It.IsAny<int>()));
+        }
+    }
+}
