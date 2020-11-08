d5e7ac8f7b93aef4ced0a024526fca4de42eb813
Patryk Wyprzał <patryk836@o2.pl>
2020-11-08 14:59:22 +0100

    ShopMVC-AddHtmlHelperExtension-IsActive

---
+++ src/Webs/ShopMVC/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopMVC.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string IsActive(this IHtmlHelper htmlHelper, string action, string controller, string cssClass = "active")
+        {
+            string currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
+            string currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+
+            return currentAction.Equals(action) && currentController.Equals(controller) ?
+                cssClass : String.Empty;
+        }
+    }
+}
