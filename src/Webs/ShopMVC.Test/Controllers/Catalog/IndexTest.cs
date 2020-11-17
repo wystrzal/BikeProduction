52d1cf356a077cb9752df4a00b350ebd907eaf2c
Patryk Wyprzał <patryk836@o2.pl>
2020-11-17 17:56:36 +0100

    ShopMVC-TestCatalogController

---
+++ src/Webs/ShopMVC.Test/Controllers/Catalog/IndexTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Moq;
+using ShopMVC.Controllers;
+using ShopMVC.Interfaces;
+using ShopMVC.Models;
+using ShopMVC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShopMVC.Test.Controllers.Catalog
+{
+    public class IndexTest
+    {
+        private readonly Mock<ICatalogService> catalogService;
+
+        private readonly CatalogController catalogController;
+        private readonly CatalogProductsViewModel vm;
+        private readonly List<CatalogProduct> catalogProducts;
+
+        public IndexTest()
+        {
+            catalogService = new Mock<ICatalogService>();
+            catalogController = new CatalogController(catalogService.Object);
+            vm = new CatalogProductsViewModel();
+            catalogProducts = new List<CatalogProduct>();
+        }
+
+        [Fact]
+        public async Task Index_Success()
+        {
+            //Arrange
+            catalogService.Setup(x => x.GetBrandListItem())
+                .Returns(Task.FromResult(It.IsAny<IEnumerable<SelectListItem>>()));
+
+            catalogService.Setup(x => x.GetProducts(It.IsAny<CatalogFilteringData>()))
+                .Returns(Task.FromResult(catalogProducts));
+
+            //Act
+            var action = await catalogController.Index(vm) as ViewResult;
+            var model = action.Model as CatalogProductsViewModel;
+
+            //Assert
+            Assert.NotNull(model);
+        }
+    }
+}
---
+++ src/Webs/ShopMVC.Test/Controllers/Catalog/ProductDetailTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ShopMVC.Controllers;
+using ShopMVC.Interfaces;
+using ShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShopMVC.Test.Controllers.Catalog
+{
+    public class ProductDetailTest
+    {
+        private const int id = 1;
+
+        private readonly Mock<ICatalogService> catalogService;
+
+        private readonly CatalogController catalogController;
+        private readonly CatalogProduct catalogProduct;
+
+        public ProductDetailTest()
+        {
+            catalogService = new Mock<ICatalogService>();
+            catalogController = new CatalogController(catalogService.Object);
+            catalogProduct = new CatalogProduct { Id = id };
+        }
+
+        [Fact]
+        public async Task ProductDetail_Success()
+        {
+            //Arrange
+            catalogService.Setup(x => x.GetProduct(id)).Returns(Task.FromResult(catalogProduct));
+
+            //Act
+            var action = await catalogController.ProductDetail(id) as ViewResult;
+            var model = action.Model as CatalogProduct;
+
+            //Assert
+            Assert.Equal(id, model.Id);
+        }
+    }
+}
