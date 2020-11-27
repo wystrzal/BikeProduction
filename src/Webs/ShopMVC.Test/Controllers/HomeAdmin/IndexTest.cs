e13b7f90458fc5d07d51fc23a4d11f59320de2d2
Patryk Wyprzał <patryk836@o2.pl>
2020-11-27 17:52:51 +0100

    TestHomeAdminController

---
+++ src/Webs/ShopMVC.Test/Controllers/HomeAdmin/IndexTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopMVC.Areas.Admin.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShopMVC.Test.Controllers.HomeAdmin
+{
+    public class IndexTest
+    {
+        private readonly HomeController homeController;
+
+        public IndexTest()
+        {
+            homeController = new HomeController();
+        }
+
+        [Fact]
+        public void Index_Success()
+        {
+            //Act
+            var action = homeController.Index() as ViewResult;
+
+            //Assert
+            Assert.NotNull(action);
+        }
+    }
+}
